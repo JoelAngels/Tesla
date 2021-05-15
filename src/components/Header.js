6c1a6f02da99cf3b2e6ee46336c3bae143f7a3a6
JoelAngels <joelbaraka4325@gmail.com>
2021-05-15 14:58:01 +0300

    Cleaned files, added styled components and the layout

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Lets build the header</h1>
+      <h1>Lets build the header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Section.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = () => {
+  return (
+    <Wrap>
+      {/* <h1>Section</h1>
+      <h1>Section</h1> */}
+    </Wrap>
+  );
+};
+
+export default Section;
+
+const Wrap = styled.div``;
