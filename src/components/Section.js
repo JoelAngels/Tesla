39c75b203e69e66f0cea75dd6862a9ed8c01c976
JoelAngels <joelbaraka4325@gmail.com>
2021-05-15 16:03:14 +0300

    Made the Section loadout

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,8 +4,17 @@ import styled from "styled-components";
 const Section = () => {
   return (
     <Wrap>
-      {/* <h1>Section</h1>
-      <h1>Section</h1> */}
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
     </Wrap>
   );
 };
@@ -15,5 +24,58 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw; //full view width
   height: 100vh;
-  background-color: orange;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column; //top to bottom
+  justify-content: space-between; //vertical-alignment
+  align-items: center; //horizontal alignment
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center; //its always horizontal east to west
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`; //Inherits the left button
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div`
+  //Animation to Hope,
 `;
